d8164960ab495af38a5baa6dae96d9bb70e9c099
Jorge Machado <jorgehenriqueq@gmail.com>
2025-10-07 13:09:26 -0300

    Fix(ui): Adjust skills chips for desktop mode

--- lib/widgets/skills_desktop.dart
+++ lib/widgets/skills_desktop.dart
@@ -11,110 +11,119 @@ class SkillsDesktop extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
     final screenWidth = screenSize.width;
 
-    
     return Container(
-                width: screenWidth,
-                padding: EdgeInsets.fromLTRB(25, 20, 25, 60),
-                color: CustomColor.bgLight1,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+      width: screenWidth,
+      padding: EdgeInsets.fromLTRB(25, 20, 25, 60),
+      color: CustomColor.bgLight1,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          //title
+          Text(
+            'What I can do',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: CustomColor.whitePrimary,
+            ),
+          ),
+          Gap(20),
+
+          //plataforms and skills
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //platforms
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 450),
+                child: Wrap(
+                  spacing: 5,
+                  runSpacing: 5,
                   children: [
-                    //title
-                    Text(
-                      'What I can do',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: CustomColor.whitePrimary,
-                      ),
-                    ),
-                    Gap(20),
-
-                    //plataforms and skills
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        //platforms
-                        ConstrainedBox(
-                          constraints: BoxConstraints(maxWidth: 450),
-                          child: Wrap(
-                            spacing: 5,
-                            runSpacing: 5,
-                            children: [
-                              for (int i = 0; i < platformItems.length; i++)
-                                Container(
-                                  width: 200,
-                                  decoration: BoxDecoration(
-                                    color: CustomColor.bgLight2,
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: ListTile(
-                                    contentPadding: EdgeInsets.symmetric(
-                                      horizontal: 20,
-                                      vertical: 10,
-                                    ),
-                                    leading: Image.asset(
-                                      platformItems[i]['img'],
-                                      width: 26,
-                                    ),
-                                    title: Text(platformItems[i]['title'], style: TextStyle(color: CustomColor.whitePrimary, fontWeight: FontWeight.bold),),
-                                  ),
-                                ),
-                            ],
-                          ),
+                    for (int i = 0; i < platformItems.length; i++)
+                      Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: CustomColor.bgLight2,
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        Gap(50),
-
-                        //skills
-                        Flexible(
-                          child: Container(
-                            constraints: BoxConstraints(maxWidth: 450),
-                            child: LayoutBuilder(
-                              builder: (context, constraints) {
-                                int crossAxisCount;
-                                if (constraints.maxWidth < 450) {
-                                  crossAxisCount = 2;
-                                } else if (constraints.maxWidth < 700) {
-                                  crossAxisCount = 3;
-                                } else {
-                                  crossAxisCount = 4;
-                                }
-
-                                return GridView.count(
-                                  crossAxisCount: crossAxisCount,
-                                  mainAxisSpacing: 10,
-                                  crossAxisSpacing: 10,
-                                  childAspectRatio:
-                                      3.5,
-                                  shrinkWrap: true,
-                                  physics: const NeverScrollableScrollPhysics(),
-
-                                  children: List.generate(skillItems.length, (
-                                    i,
-                                  ) {
-                                    return Chip(
-                                      backgroundColor: CustomColor.bgLight2,
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 12,
-                                        horizontal: 16,
-                                      ),
-                                      label: Text(skillItems[i]['title'], style: TextStyle(color: CustomColor.whitePrimary, fontWeight: FontWeight.bold),),
-                                      avatar: Image.asset(
-                                        skillItems[i]['img'],
-                                        width: 26,
-                                      ),
-                                    );
-                                  }),
-                                );
-                              },
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          leading: Image.asset(
+                            platformItems[i]['img'],
+                            width: 26,
+                          ),
+                          title: Text(
+                            platformItems[i]['title'],
+                            style: TextStyle(
+                              color: CustomColor.whitePrimary,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
                   ],
                 ),
-              );
+              ),
+              Gap(50),
+
+              //skills
+              Container(
+                constraints: const BoxConstraints(maxWidth: 500),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    int crossAxisCount;
+                    if (constraints.maxWidth < 300) {
+                      crossAxisCount = 2;
+                    } else if (constraints.maxWidth < 450) {
+                      crossAxisCount = 2;
+                    } else {
+                      crossAxisCount = 4;
+                    }
+
+                    return GridView.count(
+                      crossAxisCount: crossAxisCount,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      childAspectRatio: 1.8,
+
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+
+                      children: List.generate(skillItems.length, (i) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: CustomColor.bgLight2,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(skillItems[i]['img'], width: 22),
+                              const Gap(8),
+                              Text(
+                                skillItems[i]['title'],
+                                style: const TextStyle(
+                                  color: CustomColor.whitePrimary,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
